d1a55ad0a23585f9343724b9beb1eb54f1884127
technomil <emilie.lupien-durocher@cssp.gouv.qc.ca>
2025-03-17 22:31:49 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,44 @@
-basic.forever(function () {
-    for (let index = 0; index < 4; index++) {
-        basic.showLeds(`
-            # . # . .
-            # # # # #
-            . . # . #
-            . # # . .
-            . # . # .
-            `)
-        basic.pause(100)
-        basic.showLeds(`
-            . . # . #
-            # # # # #
-            # . # . .
-            . # # . .
-            . # . # .
-            `)
-    }
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . # . #
+        . # . # .
+        `)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        # . . . #
+        `)
+})
+basic.showLeds(`
+    . # # # .
+    . # . # .
+    . # . # .
+    . # . # .
+    . # # # .
+    `)
+
